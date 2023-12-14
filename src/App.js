2724641f14e5307436b38024debad7a1d02ed629
3ichael7ambert <mlambertbusiness@gmail.com>
2023-12-14 15:40:06 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Pokedex from './Pokedex';
+import logo from './logo.svg';
+import './App.css';
+import './Pokecard.css';
+import './Pokedex.css';
+
+function App() {
+  return (
+        <div className="App">
+      <h1>Pokedex</h1>
+      <Pokedex />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Pokecard = ({ id, name, type, base_experience }) => {
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+  return (
+    <div className="Pokecard">
+      <h3>{name}</h3>
+      <img src={imgUrl} alt={name} />
+      <p>Type: {type}</p>
+      <p>Base Experience: {base_experience}</p>
+    </div>
+  );
+};
+
+export default Pokecard;
